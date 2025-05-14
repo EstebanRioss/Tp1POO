8f0c173a56f50379100c0dd031851b2a54ad6c38
Esteban <esteban_690@hotmail.com>
2025-05-14 11:46:45 -0300

    Create Rol.cs
    creacion clases

---
+++ TP1LPOO/ClassLibrary1/ClassLibrary1/Rol.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public class Rol
+    {
+        private string ROL_Codigo;
+
+        // Propiedad pública
+        public string ROL_Descripcion { get; set; }
+
+        // Constructor
+        public void Rol(string codigo, string descripcion)
+        {
+            this.ROL_Codigo = codigo;
+            this.ROL_Descripcion = descripcion;
+        }
+
+        public string GetCodigo(){
+            return this.ROL_Codigo;
+        }
+        public void SetCodigo(string codigo) {
+            this.ROL_Codigo = codigo;
+        }
+    }
+}
