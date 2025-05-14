cf1180b56837175f9bc7958cbb84315c8344dc37
Esteban <esteban_690@hotmail.com>
2025-05-14 11:46:15 -0300

    Create Destino.cs
    creacion clases

---
+++ TP1LPOO/ClassLibrary1/ClassLibrary1/Destino.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public class Destino
+    {
+        // Atributo privado
+        private int DES_Codigo;
+
+        // Propiedad pública
+        public string DES_Descripcion { get; set; }
+
+        // Constructor
+        public void Destino(int codigo, string descripcion)
+        {
+            this.DES_Codigo = codigo;
+            this.DES_Descripcion = descripcion;
+        }
+
+        public int GetCodigo(){
+            return this.DES_Codigo;
+        }
+        public void SetCodigo(int codigo) {
+            this.DES_Codigo = codigo;
+        }
+    }
+}
