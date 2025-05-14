2af1b53a9122325c97448bbf13a468e544639964
Esteban <esteban_690@hotmail.com>
2025-05-14 11:46:36 -0300

    Create Pago.cs
    creacion clases

---
+++ TP1LPOO/ClassLibrary1/ClassLibrary1/Pago.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public class Pago
+    {
+         // Atributo privado
+        private int PAG_Codigo;
+
+        // Atributo protegido
+        protected int CUO_Codigo;
+
+        // Propiedades públicas
+        public DateTime PAG_Fecha { get; set; }
+        public decimal PAG_Importe { get; set; }
+
+        // Constructor
+        public void Pago(int codigo, int cuoCodigo)
+        {
+            this.PAG_Codigo = codigo;
+            this.CUO_Codigo = cuoCodigo;
+        }
+
+        public int GetCodigo(){
+            return this.CUO_Codigo;
+        }
+        public void SetCodigo(int codigo) {
+            this.CUO_Codigo = codigo;
+        }
+    }
+}
