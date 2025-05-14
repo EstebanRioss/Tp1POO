c0dc1a5f0980e4fdd15b50ca4bd9f0739c5457a9
Esteban <esteban_690@hotmail.com>
2025-05-14 11:46:50 -0300

    Create Usuario.cs
    creacion clases

---
+++ TP1LPOO/ClassLibrary1/ClassLibrary1/Usuario.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public class Usuario
+    {
+        private int USU_ID;
+
+        // Propiedades públicas
+        public string USU_NombreUsuario { get; set; }
+        public string USU_Contraseña { get; set; }
+        public string USU_ApellidoNombre { get; set; }
+
+        // Atributo protegido
+        protected string ROL_Codigo;
+
+        // Constructor
+        public void Usuario(int id, string rolCodigo)
+        {
+            this.USU_ID = id;
+            this.ROL_Codigo = rolCodigo;
+        }
+
+        public int GetID(){
+            return this.USU_ID;
+        }
+        public void SetID(int id) {
+            this.USU_ID = id;
+        }
+    }
+}
